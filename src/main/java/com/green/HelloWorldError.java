1b216d44aa04db194b9ac362076644c2a13a67fc
Ildar Green <igoonegreen@gmail.com>
2021-01-14 15:15:57 +0700

    Initial commit: add all? no test

---
+++ src/main/java/com/green/HelloWorldError.java
@@ -0,0 +1,26 @@
+package com.green;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorldError {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldError.class);
+
+	public static void main(String[] args) {
+
+		try {
+			System.out.println(getData());
+		} catch (IllegalArgumentException e) {
+			logger.error("{}", e);
+			logger.error("{}", e);
+
+		}
+
+	}
+
+	static int getData() throws IllegalArgumentException {
+		throw new IllegalArgumentException("Sorry IllegalArgumentException!");
+	}
+
+}
